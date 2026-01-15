d9c94c144b1dfe452a7749a52d1ca3dafc030a8e
HarshCraft <harshindia8637@gmail.com>
2026-01-15 19:46:58 +0530

    commit#27

--- backend/server.js
+++ backend/server.js
@@ -2,28 +2,25 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const cors = require("cors");
-const router = express.Router();
 
 dotenv.config();
 connectDB();
 
 const app = express();
 app.use(express.json());
+
 app.use(
-	cors({
-		origin: process.env.ORIGIN,
-		methods: ["GET", "PUT", "POST", "DELETE"],
-		credentials: true,
-	})
+  cors({
+    origin: "*",
+    methods: ["GET", "PUT", "POST", "DELETE"],
+  })
 );
-app.use(router);
+
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/auctions", require("./routes/auctionRoutes"));
 app.use("/api/bids", require("./routes/bidRoutes"));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-app.listen(5000, "0.0.0.0", () => {
-    console.log("Server running on port 5000");
-});
+app.listen(PORT, "0.0.0.0", () =>
+  console.log(`Server running on port ${PORT}`)
+);
